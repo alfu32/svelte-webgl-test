e58dc67773d97c074f0b17dd322d098103a9cf8e
alfu32 <ioan.alferaru@gmail.com>
2023-11-07 11:58:25 +0100

    Initial commit

---
+++ src/lib/WebglView.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import {onMount} from "svelte"
+  let canvas:HTMLCanvasElement
+  onMount(()=>{
+    
+  })
+</script>
+
+<canvas bind:this={canvas}>
+  Please use a browser that supports "canvas"
+</canvas>
